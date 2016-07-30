239ec7b7658ceaff8b34f870b105a8fb8119b3a8
Nguyen Khang <ntnk1984@me.com>
2016-07-30 11:03:17 +0700

    Initial Commit

---
+++ DatVeXemPhim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatVeXemPhim
+//
+//  Created by ngolehoang on 7/30/16.
+//  Copyright © 2016 VNPOST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DatVeXemPhim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DatVeXemPhim
+//
+//  Created by ngolehoang on 7/30/16.
+//  Copyright © 2016 VNPOST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
